8f44a5d06e414363a825dbc6726543d1ba506a78
phuc-noob <phucnooob@gmail.com>
2020-06-17 06:31:56 +0700

    char and boolean in java .

---
+++ CharAndBoolean/src/learnJava/masterClass/Main.java
@@ -0,0 +1,23 @@
+package learnJava.masterClass;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    char myCharacter = 'D';
+
+	    // unicode-table.com
+	    char myUnicodeChar ='\u0044';
+        char myCopyRightChar = '\u00A9';
+
+        System.out.println(myCharacter);
+        System.out.println(myUnicodeChar);
+        System.out.println(myCopyRightChar);
+
+        // Boolean primitive
+        boolean trueValue = true;
+        boolean falseValue = false;
+
+        System.out.println(trueValue);
+        System.out.println(falseValue);
+    }
+}
